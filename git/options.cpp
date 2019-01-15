8c97d48dc7a2b1c585fbcdcea446e536855fde21
Nick Dugger <email@nickdugger.com>
2019-01-15 10:02:33 -0600

    Update options.cpp

--- git/options.cpp
+++ git/options.cpp
@@ -8,7 +8,7 @@
 # include "git2/checkout.h"
 
 namespace {
-    typedef options_type = std::initializer_list<std::pair<std::string, std::variant<bool, int, std::string>>>;
+    typedef git_options_type = std::initializer_list<std::pair<std::string, std::variant<bool, int, std::string>>>;
 }
 
 namespace git::options {
@@ -19,7 +19,7 @@ namespace git::options {
             git_checkout_opts checkout_options_c_obj;
         
         protected:
-            explicit checkout (options_type options) : checkout_options_c_obj(GIT_CHECKOUT_OPTIONS_INIT) {
+            explicit checkout (git_options_type options) : checkout_options_c_obj(GIT_CHECKOUT_OPTIONS_INIT) {
                 // TODO lots of logic to map options to checkout_options_c_obj
             }
     };
